83eb32431928c419838b3e6b0153a3c1ca40b312
montekaka <josh.chen.jia@gmail.com>
2018-10-27 16:50:33 -0700

    handle cross site request

--- server/app.js
+++ server/app.js
@@ -7,6 +7,12 @@ const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+
 
 const port = 8080;
 

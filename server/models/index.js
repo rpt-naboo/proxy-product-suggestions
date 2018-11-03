f8531d3f80cdbe1761bec58fc0478889250c5f89
montekaka <josh.chen.jia@gmail.com>
2018-11-03 11:41:32 -0700

    remove the console log to print out env

--- server/models/index.js
+++ server/models/index.js
@@ -7,7 +7,6 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const configs = require(__dirname + '/../config/config.js');
 const config = configs[env];
-console.log(env, config)
 // const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 

93a18aba983cf1bae6b57fca1606877c50ce9ca3
montekaka <josh.chen.jia@gmail.com>
2018-11-04 23:23:00 -0800

    add pm2 deployment script to config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   apps: [{
-    script: "server/app.js",
-    instances: "max",
-  }]
+  	name: 'rpt-08-naboo',
+    script: "server/app.js"
+  }],
+  deploy: {
+  	production: {
+      user: 'ubuntu',
+      host: 'ec2-13-56-227-30.us-west-1.compute.amazonaws.com',
+      key: '~/.ssh/rpt-08-naboo.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:rpt-naboo/proxy-product-suggestions.git',
+      path: '/home/ubuntu/rpt-08-naboo',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
 }

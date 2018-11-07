150bffa886dadcd96b4cfbe282303adf8514c016
montekaka <josh.chen.jia@gmail.com>
2018-11-06 20:55:59 -0800

    add ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps: [{
+    name: 'proxy-product-suggestions',
+    script: "server/app.js",
+    env: {
+      "NODE_ENV": "production",
+      "DB_NAME": "naboo",
+      "DB_USER_NAME": "naboo",
+      "DB_HOST": "naboo.ce2dneemekr4.us-east-1.rds.amazonaws.com",
+      "DB_PASSWORD": "rpt-08-naboo-ms"
+    }
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-13-56-227-30.us-west-1.compute.amazonaws.com',
+      key: '~/.ssh/rpt-08-naboo.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:rpt-naboo/proxy-product-suggestions.git',
+      path: '/home/ubuntu/rpt-08-naboo',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}

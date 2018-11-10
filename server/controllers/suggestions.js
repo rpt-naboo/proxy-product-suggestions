f3cb2851537eb893cbeaf663f3ff24328f6ca4c8
montekaka <josh.chen.jia@gmail.com>
2018-11-10 12:05:13 -0800

    add create suggestion endpoint

--- server/controllers/suggestions.js
+++ server/controllers/suggestions.js
@@ -1,4 +1,5 @@
 const Suggestion = require('../models').Suggestion;
+const productSimilarity = require('./../../libs/helpers/productSimilarity');
 
 module.exports = {
   list(req, res) {
@@ -14,5 +15,19 @@ module.exports = {
         res.status(200).send(suggestions);
       })
       .catch(error => res.status(400).send(error));
+  },
+  create(req, res) {
+    const postBody = req.body;    
+    const product1_id = postBody.product1_id;
+    const product2_id = postBody.product2_id;
+    const product1_text = postBody.product1_text;
+    const product2_text = postBody.product2_text;        
+    const productsSimilarity = productSimilarity(product1_id, product1_text, product2_id, product2_text);
+
+    Suggestion.bulkCreate(productsSimilarity)
+    .then(() => {
+      res.status(200).send('created new items');
+    })
+    .catch(err => res.status(400).send(error));
   }
 };
